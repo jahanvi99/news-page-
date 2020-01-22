8b716b66f7b532c74ad67f0f4156b79e2e7d7fc9
jahanvi99 <30996072+jahanvi99@users.noreply.github.com>
2020-01-22 12:48:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
 import "./styles/App.css";
-import search from "./Components/search";
+import Search from "./Components/search";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Search Results</h1>
-        </header>
-        <p className="App-intro">{search}</p>
-      </div>
-    );
+    return <Search/>;
   }
 }
 
